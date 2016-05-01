0bb4cfcc63bce602d148d4991ba6bcc5f88ecee5
sNAtttu <nsanteri@hotmail.com>
2016-05-01 18:53:22 +0300

    Press H for help

--- PhaserExample/Level1.ts
+++ PhaserExample/Level1.ts
@@ -5,11 +5,18 @@
         background: Phaser.Sprite;
         music: Phaser.Sound;
         player: Miner.Player;
+        helpShown: boolean;
+        helpText: string;
+        helpStyle: {};
+        helpDomElement: Phaser.Text;
 
         create() {
-
+            this.helpText = "M: Stop music, Arrow keys: Movement, Space: Try to find lewt";
+            this.helpStyle = { font: "36px Arial", fill: "#ffffff", align: "center" };
+            this.input.keyboard.addKey(Phaser.Keyboard.H).onDown.add(this.showHelpMenu, this);
             this.input.keyboard.addKey(Phaser.Keyboard.M).onDown.add(this.stopMusic, this);
             this.game.add.graphics(100, 40);
+            this.helpShown = false;
             this.background = this.add.sprite(0, 0, 'level1');
             this.add.tween(this.background);
             this.music = this.add.audio('gameMusic',1, true);
@@ -24,6 +31,18 @@
             }
             else {
                 this.music.play();
+            }
+        }
+        showHelpMenu() {
+            if (this.helpShown) {
+                this.game.world.remove(this.helpDomElement);  
+                console.log("piilota");
+                this.helpShown = false;
+            }
+            else {
+                this.helpDomElement = this.game.add.text(0, 0, this.helpText, this.helpStyle);
+                console.log("näytä");
+                this.helpShown = true;
             }
         }
 
--- PhaserExample/game.js
+++ PhaserExample/game.js
@@ -35,8 +35,12 @@ var Miner;
 (function (Miner) {
     class Level1 extends Phaser.State {
         create() {
+            this.helpText = "M: Stop music, Arrow keys: Movement, Space: Try to find lewt";
+            this.helpStyle = { font: "36px Arial", fill: "#ffffff", align: "center" };
+            this.input.keyboard.addKey(Phaser.Keyboard.H).onDown.add(this.showHelpMenu, this);
             this.input.keyboard.addKey(Phaser.Keyboard.M).onDown.add(this.stopMusic, this);
             this.game.add.graphics(100, 40);
+            this.helpShown = false;
             this.background = this.add.sprite(0, 0, 'level1');
             this.add.tween(this.background);
             this.music = this.add.audio('gameMusic', 1, true);
@@ -49,6 +53,18 @@ var Miner;
             }
             else {
                 this.music.play();
+            }
+        }
+        showHelpMenu() {
+            if (this.helpShown) {
+                this.game.world.remove(this.helpDomElement);
+                console.log("piilota");
+                this.helpShown = false;
+            }
+            else {
+                this.helpDomElement = this.game.add.text(0, 0, this.helpText, this.helpStyle);
+                console.log("näytä");
+                this.helpShown = true;
             }
         }
     }
